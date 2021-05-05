3c5810386f49434bf169d7b5a46d2c00740dc145
Will Warner <will.r.warner@gmail.com>
2021-05-05 06:08:53 -0500

    chore: export parse function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import antlr4 from "antlr4";
 import PlSqlLexer from "../antlr4-grammars/PlSqlLexer.js";
 import PlSqlParser from "../antlr4-grammars/PlSqlParser.js";
 
-let code = "select * from test;";
-code = code.toUpperCase();
-const chars = new antlr4.InputStream(code);
-const lexer = new PlSqlLexer(chars);
-const tokens = new antlr4.CommonTokenStream(lexer);
-const parser = new PlSqlParser(tokens);
-parser.buildParseTrees = true;
-const tree = parser.sql_script();
-console.log(tree);
+export function parse(code) {
+  code = code.toUpperCase();
+
+  const chars = new antlr4.InputStream(code);
+  const lexer = new PlSqlLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new PlSqlParser(tokens);
+  parser.buildParseTrees = true;
+  const tree = parser.sql_script();
+
+  return tree;
+}
